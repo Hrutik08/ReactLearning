f2905be588647eae41cc7d6607bad6970d51cf70
Hrutik08 <hrutikmhatre.mhatre.com@gmail.com>
2023-07-15 16:10:42 +0530

    component added

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
-function Greet() {
-    return <h1>Hello wolrd</h1>
+// function Greet() {
+//     return <h1>Hello wolrd</h1>
+// }
+
+const Greet = () =>{
+    return <h1> Hello function </h1>
 }
 
 export default Greet
